a790d690119f68cc2a142b01ed078bf0f0485d43
Robyn <rnixon98@gmail.com>
2024-08-20 20:22:01 -0500

    fix: Refuse null patches

--- src/UnityDebuggerAssistant/Utils/MonoModPatchListener.cs
+++ src/UnityDebuggerAssistant/Utils/MonoModPatchListener.cs
@@ -9,6 +9,19 @@ public static class MonoModPatchListener
 {
     internal static bool ListenForPatch(ILHook hook, MethodBase @base, ILContext.Manipulator manipulator)
     {
+
+        if (@base is null || manipulator is null || hook is null)
+        {
+            Plugin.Log.LogInfo("Skipping a null/empty patch");
+            return true;
+        }
+
+        /*
+        Plugin.Log.LogInfo(hook.Manipulator);
+        Plugin.Log.LogInfo(@base.Name);
+        Plugin.Log.LogInfo(manipulator);
+        */
+
         PatchStorage.AddPatchInformation(@base, manipulator.Target.GetType().Assembly);
         return true;
     }
